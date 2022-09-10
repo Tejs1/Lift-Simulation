89a29423c32c488282b87320a7e1f3b80641c7ba
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-09-10 10:48:00 +0530

    Trying Running two task parallelly

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ const buttons = document.querySelectorAll("button");
 let isMoving = false;
 const TOTALLIFTS = [];
 
-{
-  lifts.forEach((lift) => {
-    TOTALLIFTS.push(lift);
-    lift.addEventListener("transitionend", (e) => {
-      e.target.classList.remove("animate");
-      isMoving = false;
-    });
+lifts.forEach((lift) => {
+  TOTALLIFTS.push(lift);
+  lift.addEventListener("transitionend", (e) => {
+    e.target.classList.remove("animate");
+    console.log("end anim");
+    isMoving = false;
   });
-}
+});
+
 TOTALLIFTS[1].style.backgroundColor = "red";
 console.log(TOTALLIFTS);
 
@@ -30,28 +30,33 @@ buttons.forEach((btn) =>
     const btnDirection = e.target.className;
     const destination = getCurrentControllerFloor(e.target);
     console.log("destination" + destination);
-    moveLift(destination, 1);
+    async function calllifts() {
+      await moveLift(3, 1);
+      await moveLift(2, 0);
+    }
+    calllifts();
   })
 );
 
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 function moveLift(destination, liftNo) {
-  console.log("movelift" + destination + "" + liftNo);
-  const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   (async () => {
     for (i = 0; true; i++) {
-      console.log("currentfloorlift" + currentLiftFloor(liftNo));
-      if (destination > currentLiftFloor(liftNo)) {
-        moveUp(liftNo);
+      if (!TOTALLIFTS[1].className.includes("animate")) {
+        console.log("loop");
+        if (destination > currentLiftFloor(liftNo)) {
+          moveUp(liftNo);
+        }
+        if (destination < currentLiftFloor(liftNo)) {
+          console.log("movedown");
+          moveDown(liftNo);
+        }
+        if (destination == currentLiftFloor(liftNo)) {
+          return;
+        }
       }
-      if (destination < currentLiftFloor(liftNo)) {
-        console.log("movedown");
-        moveDown(liftNo);
-      }
-      if (destination == currentLiftFloor(liftNo)) {
-        return;
-      }
-      await wait(1000);
-      console.log("loop");
+
+      await wait(10);
     }
   })();
 }
@@ -98,3 +103,52 @@ function moveUp(liftNo) {
   flip.invert(TOTALLIFTS[liftNo]);
   flip.play(TOTALLIFTS[liftNo]);
 }
+
+// class ConcurrentTaskQueue {
+//   constructor(taskPromisesFunc = [], batchSize = 1) {
+//     this.batchSize =
+//       batchSize > taskPromisesFunc.length ? taskPromisesFunc.length : batchSize;
+//     this.todoTasks = taskPromisesFunc;
+//     this.resolvedValues = [];
+//   }
+
+//   run(resolve, reject) {
+//     if (this.todoTasks.length > 0) {
+//       const taskPromises = this.todoTasks.splice(0, this.batchSize);
+//       Promise.all(taskPromises.map((p) => p()))
+//         .then((resolvedValues) => {
+//           this.resolvedValues = [...this.resolvedValues, ...resolvedValues];
+//           this.run(resolve, reject);
+//         })
+//         .catch((err) => reject(err));
+//     } else {
+//       resolve(this.resolvedValues);
+//     }
+//   }
+
+//   runTasks() {
+//     return new Promise((resolve, reject) => {
+//       this.run(resolve, reject);
+//     });
+//   }
+// }
+
+// // some arbitrary function that consumes resources
+// const costlyFunction = (arg) =>
+//   new Promise((resolve) => {
+//     // do something costly here
+//     resolve(arg);
+//   });
+
+// const batchSize = 2;
+// const taskQueue = new ConcurrentTaskQueue(
+//   [
+//     // wrap all functions to prevent direct execution
+//     () => moveLift(2, 0),
+//     () => moveLift(3, 1),
+//   ],
+//   batchSize
+// );
+// taskQueue.runTasks().then(([res1, res2]) => {
+//   console.log(res1, res2);
+// });
