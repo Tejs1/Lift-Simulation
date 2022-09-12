769b94fb3caf3e9a7c0b58195f47977fe492f81f
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2022-09-12 22:35:24 +0530

    Added multiple move lift func

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-const floors = document.querySelectorAll(".floor");
+const floors = document.querySelectorAll(".lift-path");
 const lifts = document.querySelectorAll(".lift");
 const buttons = document.querySelectorAll("button");
-let isMoving = false;
-const TOTALLIFTS = [];
+const log = console.log
 
-lifts.forEach((lift) => {
+const TOTALLIFTS = [];
+let idleLifts = []
+const allFlips = {}
+lifts.forEach((lift, idx) => {
   TOTALLIFTS.push(lift);
+  lift.style.left = `${100/lifts.length * idx}%`
   lift.addEventListener("transitionend", (e) => {
-    e.target.classList.remove("animate");
+    idleLifts.push(e.target)
+    e.target.style.transition = `initial`;
     console.log("end anim");
-    isMoving = false;
+    e.target.classList.remove("moving");
   });
 });
 
-TOTALLIFTS[1].style.backgroundColor = "red";
-console.log(TOTALLIFTS);
 
-const currentLiftFloor = (liftNo) => {
-  return Number(TOTALLIFTS[liftNo].parentNode.className.match(/\d/g));
+
+
+const currentLiftFloor = (lift) => {
+  return  Number(lift.parentNode.parentNode.className.match(/\d/g))
 };
 
 const getCurrentControllerFloor = (element) => {
@@ -25,136 +29,78 @@ const getCurrentControllerFloor = (element) => {
   //extracts number from className
   return Number(currentClassName.match(/\d/g));
 };
+
+
 buttons.forEach((btn) =>
   btn.addEventListener("click", (e) => {
     const btnDirection = e.target.className;
     const destination = getCurrentControllerFloor(e.target);
     console.log("destination" + destination);
-    if (true) {
-      moveLift(destination, 0);
-    } else {
-    }
-    // moveLift(destination, 1);
-    // async function calllifts() {
-    //   await moveLift(3, 1);
-    //   await moveLift(2, 0);
-    // }
-    // calllifts();
+    log("nearlift " +getLift(destination))
+    moveLift(destination, getLift(destination))
   })
 );
-// create a destination array
-// destination [0,1,2,]
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-function moveLift(destination, liftNo) {
-  (async () => {
-    for (i = 0; true; i++) {
-      if (!TOTALLIFTS[1].className.includes("animate")) {
-        console.log("loop");
-        if (destination > currentLiftFloor(liftNo)) {
-          moveUp(liftNo);
-        }
-        if (destination < currentLiftFloor(liftNo)) {
-          console.log("movedown");
-          moveDown(liftNo);
-        }
-        if (destination == currentLiftFloor(liftNo)) {
-          return;
-        }
-      }
 
-      await wait(10);
-    }
-  })();
+function getShortestDistance( lift ,destination, j  ){  
+  if(Math.abs(currentLiftFloor(lift) - destination) === 0){
+    log( j  + "seeting")
+    return j
+  }
 }
+
+
+function getLift(destination){
+   let liftNo = false;
+   
+   for(let i=0; i< floors.length; i++){
+    // if(liftNo) return liftNo
+    for(j=0 ; j < TOTALLIFTS.length; j++){
+      if(liftNo  || liftNo === 0) return liftNo
+      if(!TOTALLIFTS[j].classList.contains("moving")){
+        if(Math.abs(Math.abs(currentLiftFloor(TOTALLIFTS[j]) - destination)) === i){
+          liftNo = j      
+        }
+      }   
+    }
+  }
+  log(liftNo)
+  return liftNo
+ }
+
+function moveLift(destination, liftNo) {
+  if (!TOTALLIFTS[liftNo].className.includes("moving")) {
+  flip.first(TOTALLIFTS[liftNo]);
+  flip.append(destination ,TOTALLIFTS[liftNo]); //append
+  const delta = flip.invert(TOTALLIFTS[liftNo]);
+  flip.play(TOTALLIFTS[liftNo],delta);
+  }
+ }
+
 
 //flip animation
 class Flip {
   first(el) {
+    el.classList.add("moving")
     this.state = el.getBoundingClientRect();
   }
 
+  append(destination,el){
+    floors[destination].appendChild(el)
+  }
   invert(el) {
     const first = this.state;
     const last = el.getBoundingClientRect();
     let delta = -1 * (last.top - first.top);
     el.style.transform = `translateY(${delta}px)`;
+    return delta
   }
 
-  play(el) {
+  play(el ,delta) {
     requestAnimationFrame(function () {
-      el.classList.add("animate");
+      el.style.transition = `all ${Math.abs(delta * 2)}ms ease-in-out`;
       el.style.transform = "none";
     });
   }
 }
 
-const flip = new Flip();
-
-function moveDown(liftNo) {
-  if (isMoving) return;
-  isMoving = true;
-  const goTo = floors[currentLiftFloor(liftNo) - 1];
-  flip.first(TOTALLIFTS[liftNo]);
-  goTo.appendChild(TOTALLIFTS[liftNo]); //append
-  flip.invert(TOTALLIFTS[liftNo]);
-  flip.play(TOTALLIFTS[liftNo]);
-}
-
-function moveUp(liftNo) {
-  if (isMoving) return;
-  isMoving = true;
-  const goTo = floors[currentLiftFloor(liftNo) + 1];
-  flip.first(TOTALLIFTS[liftNo]);
-  goTo.appendChild(TOTALLIFTS[liftNo]); //append
-  flip.invert(TOTALLIFTS[liftNo]);
-  flip.play(TOTALLIFTS[liftNo]);
-}
-
-// class ConcurrentTaskQueue {
-//   constructor(taskPromisesFunc = [], batchSize = 1) {
-//     this.batchSize =
-//       batchSize > taskPromisesFunc.length ? taskPromisesFunc.length : batchSize;
-//     this.todoTasks = taskPromisesFunc;
-//     this.resolvedValues = [];
-//   }
-
-//   run(resolve, reject) {
-//     if (this.todoTasks.length > 0) {
-//       const taskPromises = this.todoTasks.splice(0, this.batchSize);
-//       Promise.all(taskPromises.map((p) => p()))
-//         .then((resolvedValues) => {
-//           this.resolvedValues = [...this.resolvedValues, ...resolvedValues];
-//           this.run(resolve, reject);
-//         })
-//         .catch((err) => reject(err));
-//     } else {
-//       resolve(this.resolvedValues);
-//     }
-//   }
-
-//   runTasks() {
-//     return new Promise((resolve, reject) => {
-//       this.run(resolve, reject);
-//     });
-//   }
-// }
-
-// // some arbitrary function that consumes resources
-// const costlyFunction = (arg) =>
-//   new Promise((resolve) => {
-//     // do something costly here
-//     resolve(arg);
-//   });
-
-// const batchSize = 2;
-// const taskQueue = new ConcurrentTaskQueue(
-//   [
-//     // wrap all functions to prevent direct execution
-//     () => moveLift(2, 0),
-//     () => moveLift(3, 1),
-//   ],
-//   batchSize
-// );
-// taskQueue.runTasks().then(([res1, res2]) => {
-//   console.log(res1, res2);
-// });
+const flip = new Flip()
